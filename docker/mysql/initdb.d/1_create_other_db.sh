1834c79f37f8e05315c00d3e9812057e0f0728cd
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-11-16 22:05:44 +0900

    composer2対応（アップデートするだけ）
    DBの初期化スクリプトにテスト用のDBの生成を含めるよう修正

--- docker/mysql/initdb.d/1_create_other_db.sh
+++ docker/mysql/initdb.d/1_create_other_db.sh
@@ -4,6 +4,8 @@ mysql=( mysql --protocol=socket -u root -p"${MYSQL_ROOT_PASSWORD}" )
 
 "${mysql[@]}" <<-EOSQL
     FLUSH PRIVILEGES;
+    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}_test;
+    GRANT ALL ON ${MYSQL_DATABASE}_test.* TO '$MYSQL_USER'@'%';
     CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}_debug;
     GRANT ALL ON ${MYSQL_DATABASE}_debug.* TO '$MYSQL_USER'@'%';
 EOSQL
